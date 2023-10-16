e1df71fc1d498e3a2c5fb4e6300bf8b9d49d3f22
harshvachhani1 <harshv@webelignt.co.in>
2023-10-16 18:15:36 +0530

    creating a component

---
+++ components/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>{{ msg }}</p>
+</template>
+<script>
+// module
+// name : anyone
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "hello World!",
+    };
+  },
+};
+</script>
