ddeb463efce80c83df2d4b0844a0de17f81f568c
harshvachhani1 <harshv@webelignt.co.in>
2023-10-17 17:53:34 +0530

    create new component,create child component, applied component style,used SASS component, describe propes,propes limitation performed, access parent to chiled component, Component Events used

---
+++ components/src/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <greeting></greeting>
+  <!-- <p>{{ msg }}</p> -->
+</template>
+<script>
+// module
+// name : anyone
+import Greeting from "@/components/Greeting.vue";
+export default {
+  name: "App",
+  components: {
+    // key(Greeting) : value(Greeting)
+    Greeting,
+  },
+};
+
+/* export default{
+name: "App";
+data(){
+  return{
+    msg : "Hello World"
+  }
+}
+ }*/
+</script>
