865bf7898c2831cc80b74306a899284c53e90b18
harshvachhani1 <harshv@webelignt.co.in>
2023-10-18 11:25:01 +0530

    explain how to use slot used slot and how to data use parent to child and also applied name slot how to worked.

---
+++ slots/src/components/Form.vue
@@ -0,0 +1,14 @@
+<template>
+  <form>
+    <!-- <slot>No Form Found.</slot> -->
+    <div class="help">
+      <slot name="help"></slot>
+    </div>
+    <div class="fields">
+      <slot name="fields"></slot>
+    </div>
+    <div class="buttons">
+      <slot name="buttons"></slot>
+    </div>
+  </form>
+</template>
