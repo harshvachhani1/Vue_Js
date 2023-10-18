a54d5763bf5aeba310356c574afbd8780fc61935
harshvachhani1 <harshv@webelignt.co.in>
2023-10-18 12:39:23 +0530

    create dynamic component used with is attribute also write keep-alive built-in component and work with lifecycle hooks

---
+++ dynamic-components/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <select v-model="componentName">
+      <!-- <option value="">select page</option> -->
+      <option value="Home">Home</option>
+      <option value="About">About</option>
+    </select>
+    <!-- unmounted lifecycle not work when we use <keep-alive> -->
+    <keep-alive>
+      <component :is="componentName"></component>
+    </keep-alive>
+  </div>
+</template>
+<script>
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+    About,
+  },
+  data() {
+    return {
+      componentName: "Home",
+    };
+  },
+};
+</script>
---
+++ dynamic-components/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  unmounted() {
+    console.log("Home component unmounted");
+  },
+  activated() {
+    console.log("Home component activated");
+  },
+  deactivated() {
+    console.log("Home component deactivated");
+  },
+};
+</script>
