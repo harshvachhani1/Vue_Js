6f3779f7d281db483b8c124c049e8c7016c6300f
harshvachhani1 <harshv@webelignt.co.in>
2023-10-24 12:34:01 +0530

    build quiz app used with yarn

---
+++ quizproject/src/components/Result.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="result">
+      <div class="title">{{ results[resultIndex].title }}</div>
+      <div class="desc">{{ results[resultIndex].desc }}</div>
+      <!-- <div class="desc">your Total currect answer {{ totalCorrect }}</div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["results", "totalCorrect"],
+  computed: {
+    resultIndex() {
+      let index = 0;
+
+      this.results.forEach((currentItem, currentIndex) => {
+        if (
+          currentItem.min <= this.totalCorrect &&
+          currentItem.max >= this.totalCorrect
+        ) {
+          index = currentIndex;
+        }
+      });
+
+      return index;
+    },
+  },
+};
+</script>
