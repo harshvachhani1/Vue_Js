62502f01e0a3dd6237d677864941662d42f748e8
harshvachhani1 <harshv@webelignt.co.in>
2023-10-26 18:05:58 +0530

    insert new validaion,submit button event code easy to understand for create component,use slot property,create login and register file

--- musicApp/src/includes/validation.js
+++ musicApp/src/includes/validation.js
@@ -1,4 +1,11 @@
-import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from 'vee-validate'
+// component and function import
+import {
+  Form as VeeForm,
+  Field as VeeField,
+  defineRule,
+  ErrorMessage,
+  configure
+} from 'vee-validate'
 import {
   required,
   min,
@@ -12,21 +19,49 @@ import {
 } from '@vee-validate/rules'
 
 export default {
+  // app argument is a reference to the vue application
   install(app) {
     app.component('VeeForm', VeeForm)
     app.component('VeeField', VeeField)
     app.component('ErrorMessage', ErrorMessage)
 
     defineRule('required', required)
+    defineRule('tos', required)
     defineRule('min', min)
     defineRule('max', max)
     defineRule('alpha_spaces', alphaSpaces)
     defineRule('email', email)
     defineRule('min_value', minValue)
     defineRule('max_value', maxValue)
-    defineRule('confirmed', confirmed)
-    defineRule('not_one_of', excluded)
+    defineRule('password_mismatch', confirmed)
+    defineRule('excluded', excluded)
+    defineRule('countryExcluded', excluded)
 
+    configure({
+      generateMessage: (contextMessage) => {
+        const messages = {
+          required: `the field ${contextMessage.field} is required.`,
+          min: `The field ${contextMessage.field}is too short.`,
+          max: `The field ${contextMessage.field} is too long.`,
+          alpha_spaces: `The field ${contextMessage.field} may only contain alphabetic characters and spaces.`,
+          email: `The field ${contextMessage.field} must be a valid email`,
+          min_value: `The field ${contextMessage.field} is too low`,
+          max_value: `The field ${contextMessage} is too high.`,
+          excluded: `You are not allowed to use this value for the field ${contextMessage.field}`,
+          countryExcluded: `Due to restrctions, we do not accept users from this locaion`,
+          password_mismatch: "The passwords don't match.",
+          tos: `You must accept the Terms of Service`
+        }
+        const message = messages[contextMessage.rule.name]
+          ? messages[contextMessage.rule.name]
+          : `The field ${contextMessage.field} is invalid.`
+        return message
+      },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true
+    })
     // app.component(name:string)
   }
 }
