dd6c74122531b76f02ed8576d87965ca6036febe
harshvachhani1 <harshv@webelignt.co.in>
2023-10-26 18:11:24 +0530

    explain how to use slot and use shorthand property

---
+++ slot-props/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <!-- use  shorthand property for v-slot -->
+    <!-- user,favorite will represent every property to the slot component -->
+    <hello-world #default="{ user, favorites }">
+      <p>Hello {{ user.name }}. I like {{ favorites[0] }}.</p>
+    </hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
